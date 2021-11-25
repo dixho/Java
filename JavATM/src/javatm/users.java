0e0430d2014357a37a771bcaeb0d68cf906a0376
Fran López <fran2002mc@gmail.com>
2021-11-25 15:59:53 +0100

    .

--- JavATM/src/javatm/users.java
+++ JavATM/src/javatm/users.java
@@ -2,8 +2,8 @@ package javatm;
 
 public class users {
 	
-	static String[] users = new String[] {"flf","test"};
-	static int[] pins = new int[] {1234,0000};
+	final static String[] users = new String[] {"flf","test"};
+	final static int[] pins = new int[] {1234,0000};
 	static int[] saldo = new int[] {1000,1};
 	static int[] efectivo = new int[] {237,1};
 }
